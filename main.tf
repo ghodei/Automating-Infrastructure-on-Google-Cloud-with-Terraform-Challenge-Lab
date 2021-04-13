5ca88a9fabe2c7418c27fdbdc40a785d14952ab0
ghodei <77273950+ghodei@users.noreply.github.com>
2021-04-13 10:45:24 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.55.0"
+    }
+  }
+}
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+module "instances" {
+
+  source     = "./modules/instances"
+
+}
+
+module "storage" {
+  source     = "./modules/storage"
+}
